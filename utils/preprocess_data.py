881d7f3ebb667dd3fa42c4896e713fe55333b931
pc2005 <philchen2005@gmail.com>
2021-01-18 22:05:20 -0500

    Added some comments.

--- utils/preprocess_data.py
+++ utils/preprocess_data.py
@@ -1,4 +1,4 @@
-import json
+# import json     # NOT used
 import numpy as np
 import pandas as pd
 from pathlib import Path
@@ -6,14 +6,28 @@ import argparse
 
 
 def preprocess(data_name):
+  """preprocess data stored in CSV format
+
+  Args:
+      data_name ([string]): URL of dataset CSV file
+
+  Returns:
+      df [pd.dataframe]: data frame of sample attributes except feature vector
+      feat [np.array]: feature matrix of all samples
+  """
+  # user_id, item_id, timestamp, state_label 
   u_list, i_list, ts_list, label_list = [], [], [], []
+
+  # comma_separated_list_of_features
   feat_l = []
+
+  # index
   idx_list = []
 
   with open(data_name) as f:
     s = next(f)
     for idx, line in enumerate(f):
-      e = line.strip().split(',')
+      e = line.strip().split(',') # tokenize into a list
       u = int(e[0])
       i = int(e[1])
 
@@ -37,15 +51,20 @@ def preprocess(data_name):
 
 
 def reindex(df, bipartite=True):
+  # clone data frame
   new_df = df.copy()
+
   if bipartite:
+    # assert user_id and item_id fully covered
     assert (df.u.max() - df.u.min() + 1 == len(df.u.unique()))
     assert (df.i.max() - df.i.min() + 1 == len(df.i.unique()))
 
+    # shift i-index to behind u-index (user+item nodes, uniquely indexed)
     upper_u = df.u.max() + 1
     new_i = df.i + upper_u
-
     new_df.i = new_i
+    
+    # shift to 1-index
     new_df.u += 1
     new_df.i += 1
     new_df.idx += 1
@@ -67,16 +86,21 @@ def run(data_name, bipartite=True):
   df, feat = preprocess(PATH)
   new_df = reindex(df, bipartite)
 
+  # shift features to 1-index
   empty = np.zeros(feat.shape[1])[np.newaxis, :]
   feat = np.vstack([empty, feat])
 
+  # feature matrix for nodes (user+item)
   max_idx = max(new_df.u.max(), new_df.i.max())
   rand_feat = np.zeros((max_idx + 1, 172))
 
+  # save data in separate files
   new_df.to_csv(OUT_DF)
   np.save(OUT_FEAT, feat)
   np.save(OUT_NODE_FEAT, rand_feat)
 
+
+### Preprocessing dataset 
 parser = argparse.ArgumentParser('Interface for TGN data preprocessing')
 parser.add_argument('--data', type=str, help='Dataset name (eg. wikipedia or reddit)',
                     default='wikipedia')
